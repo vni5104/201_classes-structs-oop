7c33a2ef6d9454b63e6bb384dd874ab625992012
Vicky <vickyn5104@gmail.com>
2025-11-12 21:59:38 -0500

    added Character class and methods

--- ClassesStructsOOP/ClassesStructsOOP/Program.cs
+++ ClassesStructsOOP/ClassesStructsOOP/Program.cs
@@ -1,10 +1,35 @@
 ﻿namespace ClassesStructsOOP
 {
+    public class Character
+    {
+        public string name;
+        public int exp = 0;
+
+        public Character()
+        {
+            name = "Not assigned";
+        }
+
+        public Character (string name)
+        {
+            this.name = name;
+        }
+
+        public void PrintStatsInfo()
+        {
+            Console.WriteLine($"Hero: {this.name} - {this.exp} EXP");
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Character hero = new Character();
+            hero.PrintStatsInfo();
+
+            Character heroine = new Character("Artoria");
+            heroine.PrintStatsInfo();
         }
     }
 }
